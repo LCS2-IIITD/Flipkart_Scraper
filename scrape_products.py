65cd3b2a87ad29188636b04e479841ddffbe14a3
Viresh Gupta <viresh16118@iiitd.ac.in>
2018-07-31 01:08:55 +0530

    Complete fast scraper for washing machines

--- scrape_products.py
+++ scrape_products.py
@@ -54,55 +54,46 @@ try:
 	#
 	####
 
-	# products = ['https://www.flipkart.com/moto-e4-plus-iron-gray-32-gb/p/itmexzbwxp3jstrg?pid=MOBEU9WRZHNDJANR&lid=LSTMOBEU9WRZHNDJANRXVMCW2&marketplace=FLIPKART&fm=neo/merchandising&iid=M_020cac4c-348b-4b74-a659-1c23dd66ffee_1_957E10L3KE_MC.MOBEU9WRZHNDJANR&ppt=CLP&ppn=CLP:motorola-mobile-phones-store&otracker=clp_pmu_v2_Moto+Mobiles+under+%E2%82%B910K_1_motor-categ-6b027_Moto+E4+Plus+%28Iron+Gray%2C+32+GB%29_motorola-mobile-phones-store_MOBEU9WRZHNDJANR_neo/merchandising_0&cid=MOBEU9WRZHNDJANR']
 	startURL = 'https://www.flipkart.com/washing-machines/pr?sid=j9e,abm,8qx&otracker=categorytree'
+	options = Options()
+	options.add_argument("--headless")
+	browser = webdriver.Firefox(firefox_options=options)
+	browser.get(startURL)
+	while(1):
+		contents = urllib.request.urlopen(startURL).read().decode('utf8')
+		t = page_data.findall(str(contents))
+		page_json = json.loads(t[0][:-1])
+		for slot in page_json['pageDataV4']['page']['data']['10003']:
+			if slot['widget']['type'] == 'PRODUCT_SUMMARY':
+				info_dict = slot['widget']['data']['products'][0]['productInfo']['value']
+				product_x = {}
+				product_x['id'] = info_dict['id']
+				product_x['item_id'] = info_dict['itemId']
+				product_x['listing_id'] = info_dict['listingId']
+				product_x['name'] = info_dict['titles']['title']
+				product_x['subtitle'] = info_dict['titles']['subtitle']
+				product_x['rating'] = info_dict['rating']
+				product_x['link'] = info_dict['smartUrl']
+				product_x['brand'] = info_dict['productBrand']
+				product_x['flipkart_assured'] = info_dict['flags']['enableFlipkartAdvantage']
+				product_x['price'] = info_dict['pricing']['mrp']['value']
+				product_x['category'] = info_dict['analyticsData']['category']
+				if products_db.search(product['id'].exists()) and products_db.search(product['id'] == product_x['id']):
+					print(product_x['id'],'already exists')
+				else:
+					products_db.insert(product_x)
+					print(product_x['id'],'newly added')
+		try:
+			btn = browser.find_element_by_xpath("//span[text()='Next']")
+			btn.click()
+			startURL = browser.current_url
+		except Exception as e:
+			print(e)
+			print('URL : ', startURL)
+			break
+	browser.close()
 
-	contents = urllib.request.urlopen(startURL).read().decode('utf8')
-	# print(contents)
-	t = page_data.findall(str(contents))
-	# print(t[0])
-	# print(len(t))
-	page_json = json.loads(t[0][:-1])
-	for slot in page_json['pageDataV4']['page']['data']['10003']:
-		if slot['widget']['type'] == 'PRODUCT_SUMMARY':
-			info_dict = slot['widget']['data']['products'][0]['productInfo']['value']
-			product_x = {}
-			product_x['id'] = info_dict['id']
-			product_x['item_id'] = info_dict['itemId']
-			product_x['listing_id'] = info_dict['listingId']
-			product_x['name'] = info_dict['titles']['title']
-			product_x['subtitle'] = info_dict['titles']['subtitle']
-			product_x['rating'] = info_dict['rating']
-			product_x['link'] = info_dict['smartUrl']
-			product_x['brand'] = info_dict['productBrand']
-			product_x['flipkart_assured'] = info_dict['flags']['enableFlipkartAdvantage']
-			product_x['price'] = info_dict['pricing']['mrp']['value']
-			product_x['category'] = info_dict['analyticsData']['category']
-			if products_db.search(product['id'].exists()) and products_db.search(product['id'] == product_x['id']):
-				print(product_x['id'],'already exists')
-			else:
-				products_db.insert(product_x)
-				print(product_x['id'],'newly added')
-	# print(st.keys())
-	# print(st['productInfo'])
-	# with open('data.pickle', 'wb') as f:
-	# 	pickle.dump(st, f)
-	# options = Options()
-	# # options.add_argument("--headless")
-	# browser = webdriver.Firefox(firefox_options=options)
-
-	# browser.get(startURL)
-	# try:
-	# 	t = browser.page_source
-	# 	jsondict = page_data.findall(t)
-	# 	# print(t)
-	# 	print(t)
-	# 	# print(jsondict)
-	# except Exception as e:
-	# 	raise e
-
-	# browser.close()
 except Exception as e:
 	print(e)
 	print(str(e))
-	raise e
+	browser.close()
