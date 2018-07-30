704bdf487fd4ce4251770b24ea3f44e687c46e58
Viresh Gupta <viresh16118@iiitd.ac.in>
2018-07-31 01:29:33 +0530

    Add some data

--- scrape_products.py
+++ scrape_products.py
@@ -53,45 +53,50 @@ try:
 	# Out of all the various kinds of ID, we will use `id` for identification
 	#
 	####
+	list_products = [
+		'https://www.flipkart.com/televisions/pr?sid=ckf,czl&marketplace=FLIPKART',
+		'https://www.flipkart.com/washing-machines/pr?sid=j9e,abm,8qx&otracker=categorytree',
+		'https://www.flipkart.com/oven-toaster-grills/pr?sid=j9e%2Cm38%2Cvr5',	
+	]
 
-	startURL = 'https://www.flipkart.com/washing-machines/pr?sid=j9e,abm,8qx&otracker=categorytree'
-	options = Options()
-	options.add_argument("--headless")
-	browser = webdriver.Firefox(firefox_options=options)
-	browser.get(startURL)
-	while(1):
-		contents = urllib.request.urlopen(startURL).read().decode('utf8')
-		t = page_data.findall(str(contents))
-		page_json = json.loads(t[0][:-1])
-		for slot in page_json['pageDataV4']['page']['data']['10003']:
-			if slot['widget']['type'] == 'PRODUCT_SUMMARY':
-				info_dict = slot['widget']['data']['products'][0]['productInfo']['value']
-				product_x = {}
-				product_x['id'] = info_dict['id']
-				product_x['item_id'] = info_dict['itemId']
-				product_x['listing_id'] = info_dict['listingId']
-				product_x['name'] = info_dict['titles']['title']
-				product_x['subtitle'] = info_dict['titles']['subtitle']
-				product_x['rating'] = info_dict['rating']
-				product_x['link'] = info_dict['smartUrl']
-				product_x['brand'] = info_dict['productBrand']
-				product_x['flipkart_assured'] = info_dict['flags']['enableFlipkartAdvantage']
-				product_x['price'] = info_dict['pricing']['mrp']['value']
-				product_x['category'] = info_dict['analyticsData']['category']
-				if products_db.search(product['id'].exists()) and products_db.search(product['id'] == product_x['id']):
-					print(product_x['id'],'already exists')
-				else:
-					products_db.insert(product_x)
-					print(product_x['id'],'newly added')
-		try:
-			btn = browser.find_element_by_xpath("//span[text()='Next']")
-			btn.click()
-			startURL = browser.current_url
-		except Exception as e:
-			print(e)
-			print('URL : ', startURL)
-			break
-	browser.close()
+	for startURL in list_products:
+		options = Options()
+		options.add_argument("--headless")
+		browser = webdriver.Firefox(firefox_options=options)
+		browser.get(startURL)
+		while(1):
+			contents = urllib.request.urlopen(startURL).read().decode('utf8')
+			t = page_data.findall(str(contents))
+			page_json = json.loads(t[0][:-1])
+			for slot in page_json['pageDataV4']['page']['data']['10003']:
+				if slot['widget']['type'] == 'PRODUCT_SUMMARY':
+					info_dict = slot['widget']['data']['products'][0]['productInfo']['value']
+					product_x = {}
+					product_x['id'] = info_dict['id']
+					product_x['item_id'] = info_dict['itemId']
+					product_x['listing_id'] = info_dict['listingId']
+					product_x['name'] = info_dict['titles']['title']
+					product_x['subtitle'] = info_dict['titles']['subtitle']
+					product_x['rating'] = info_dict['rating']
+					product_x['link'] = info_dict['smartUrl']
+					product_x['brand'] = info_dict['productBrand']
+					product_x['flipkart_assured'] = info_dict['flags']['enableFlipkartAdvantage']
+					product_x['price'] = info_dict['pricing']['mrp']['value']
+					product_x['category'] = info_dict['analyticsData']['category']
+					if products_db.search(product['id'].exists()) and products_db.search(product['id'] == product_x['id']):
+						print(product_x['id'],'already exists')
+					else:
+						products_db.insert(product_x)
+						print(product_x['id'],'newly added')
+			try:
+				btn = browser.find_element_by_xpath("//span[text()='Next']")
+				btn.click()
+				startURL = browser.current_url
+			except Exception as e:
+				print(e)
+				print('URL : ', startURL)
+				break
+		browser.close()
 
 except Exception as e:
 	print(e)
